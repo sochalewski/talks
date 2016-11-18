3fe10782fdfc2b9eda578342199c21e115d42030
Piotr Sochalewski <sochalewski@gmail.com>
2016-11-18 21:17:50 +0100

    👋

---
+++ SwiftPerformance/SwiftPerformance/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  SwiftPerformance
+//
+//  Created by Piotr Sochalewski on 01.11.2016.
+//  Copyright © 2016 Piotr Sochalewski. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ SwiftPerformance/SwiftPerformance/ViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ViewController.swift
+//  SwiftPerformance
+//
+//  Created by Piotr Sochalewski on 01.11.2016.
+//  Copyright © 2016 Piotr Sochalewski. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {}
